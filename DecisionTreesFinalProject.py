dfceefa323a478c8729c6ce5313a5ab5a5d53fd3
jqh88 <jqianh1888@icloud.com>
2022-04-07 20:16:51 -0400

    Initial commit

---
+++ DecisionTreesFinalProject.py
@@ -0,0 +1,12 @@
+'''
+1) Make decision tree
+2) produce data
+3) split data into train and test
+4) Give to IDE3 code to generate decision tree
+5) Code that tests this decision tree
+
+
+'''
+
+import numpy as np
+
