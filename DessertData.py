3b4f4b8ab004c1819f6318976aaaf94fcac23199
jqh88 <jqianh1888@icloud.com>
2022-04-11 21:28:45 -0400

    Prepare to make Dessert data

--- DessertData.py
+++ DessertData.py
@@ -5,6 +5,19 @@ class DessertData:
 
 
     '''
+    # Constants
+    CAKE = 1
+    ICECREAM = 2
+    SMOOTHIE = 3
+    MILKSHAKE = 4
+    CUPCAKECONE = 5
+
+# make all the combinations
+    # vessel, frozen, fruit,
+
+    # Build big matrix of stuff , 1:n-1 = features, Last column (Classes),
+    # create constants: cup, plate, cone,--> 0, 1, 2 or 1, 2, 3
+    # fruit --> 0, 1
 
     def __init__(self, features = None, classes = None):
         if features is None:
@@ -13,9 +26,12 @@ class DessertData:
             self.features = features      # numrows needs to match len classes
 
         if classes is None:
-            self.classes = []
+            self.classes = [DessertData.CAKE, DessertData.ICECREAM]
         else:
             self.classes = classes      # 6 classes
+
+
+
 
     def expand(self, numrows):
         pass
